dea2fdb2faafbb484f2a5d3ff336121fd874b5a4
Sean <64774860+seany-web@users.noreply.github.com>
2020-05-07 13:21:05 +1200

    added game logic
    added some logic to stop the player entering guesses outside of the guess range (1 - 10)

--- guessing_game.py
+++ guessing_game.py
@@ -1,35 +1,7 @@
-"""
-Python Web Development Techdegree
-Project 1 - Number Guessing Game
---------------------------------
-
-For this first project we will be using Workspaces. 
-
-NOTE: If you strongly prefer to work locally on your own computer, you can totally do that by clicking: File -> Download Workspace in the file menu after you fork the snapshot of this workspace.
-
-"""
-
 import random
 
 
 def start_game():
-    """Psuedo-code Hints
-    
-    When the program starts, we want to:
-    ------------------------------------
-    1. Display an intro/welcome message to the player.
-    2. Store a random number as the answer/solution.
-    3. Continuously prompt the player for a guess.
-      a. If the guess greater than the solution, display to the player "It's lower".
-      b. If the guess is less than the solution, display to the player "It's higher".
-    
-    4. Once the guess is correct, stop looping, inform the user they "Got it"
-         and show how many attempts it took them to get the correct number.
-    5. Let the player know the game is ending, or something that indicates the game is over.
-    
-    ( You can add more features/enhancements if you'd like to. )
-    """
-    # write your code inside this function.
     # print welcome message when a player starts the application
     print('?????????????????????????????????????????????????????\n\n')
     print('         WELCOME TO THE NUMBER GUESSING GAME\n\n')
@@ -48,7 +20,10 @@ def start_game():
         #prompt the player to guess a number between 1 and 10
         guess = input("I'm thinking of a number between 1 and 10... ")
         #game logic starts here
-        if int(guess) > solution:
+        if int(guess) > 10 or int(guess) < 1:
+          attempts += 1
+          print(f'{guess} is not in the range of 1 - 10. Please enter a number between 1 and 10.\n')
+        elif int(guess) > solution:
           attempts += 1
           print(f'The number I am thinking of is lower than {guess}\n')
         elif int(guess) < solution:
@@ -82,5 +57,5 @@ def start_game():
         #game logic ends here
     #end of game loop
 
-# Kick off the program by calling the start_game function.
+
 start_game()
